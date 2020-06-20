f1681921ace668c3720ccc5ad3db84a25d7d8b51
carlos erices <front.end.nicolas@gmail.com>
2020-06-19 21:58:30 -0400

    Add | first commit app disney with graphQL

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'api-webkit-disney';
+  data: any;
+  getCharacter: any;
+  loading = true;
+  error: any;
+  constructor(private apollo: Apollo) {}
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+        query: gql`
+          {
+            getCharacter {
+              id
+              name
+              description
+              image
+              movie{
+                name
+              }
+            }
+          }
+        `,
+      })
+      .valueChanges.subscribe((result: any) => {
+        this.getCharacter = result.data.getCharacter;
+        this.loading = result.loading;
+        this.error = result.error;
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// APOLLO GRAPHQL
+import { HttpClientModule } from "@angular/common/http";
+import { ApolloModule, APOLLO_OPTIONS } from "apollo-angular";
+import { HttpLinkModule, HttpLink } from "apollo-angular-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { GraphQLModule } from "./graphql.module";
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
+    GraphQLModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ApolloModule,
+    HttpLinkModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
