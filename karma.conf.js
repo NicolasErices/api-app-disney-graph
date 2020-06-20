68c6cbe76d0812a50fe706002602365f7f39b1f0
carlos erices <front.end.nicolas@gmail.com>
2020-06-19 22:08:29 -0400

    push to gitHub

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/api-webkit-disney'),
+      dir: require('path').join(__dirname, './coverage/api-app-disney'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'api-webkit-disney'`, () => {
+  it(`should have as title 'api-app-disney'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('api-webkit-disney');
+    expect(app.title).toEqual('api-app-disney');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('api-webkit-disney app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('api-app-disney app is running!');
   });
 });
